87cd09ca3ff8ba946ebffc3194c9e17029362fb4
James Wen <jrw2175@columbia.edu>
2016-04-27 18:05:43 -0400

    Extract key generation step to pre-PWS-deploy script
    - Remove need for testing directory

--- pws-concourse/concourse-app/deploy_to_pws.sh
+++ pws-concourse/concourse-app/deploy_to_pws.sh
@@ -5,4 +5,12 @@ set -eux
 cf delete-service concourse-pg -f
 cf create-service elephantsql turtle concourse-pg
 
+mkdir -p keys
+pushd keys
+  ssh-keygen -t rsa -f host_key -N ''
+  ssh-keygen -t rsa -f worker_key -N ''
+  ssh-keygen -t rsa -f session_signing_key -N ''
+  cp worker_key.pub authorized_worker_keys
+popd
+
 cf push -f manifest.yml
--- pws-concourse/concourse-app/pws_setup.sh
+++ pws-concourse/concourse-app/pws_setup.sh
@@ -4,26 +4,14 @@ set -eux
 
 postgres_source=$(echo $VCAP_SERVICES | jq '.elephantsql [0] .credentials .uri' | tr -d '"')
 
-rm -rf testing
-mkdir testing
+curl -L https://github.com/concourse/concourse/releases/download/v1.1.0/concourse_linux_amd64 -o concourse
+chmod +x concourse
 
-pushd testing
-  curl -L https://github.com/concourse/concourse/releases/download/v1.1.0/concourse_linux_amd64 -o concourse
-  chmod +x concourse
-
-  ssh-keygen -t rsa -f host_key -N ''
-  ssh-keygen -t rsa -f worker_key -N ''
-  ssh-keygen -t rsa -f session_signing_key -N ''
-
-  cp worker_key.pub authorized_worker_keys
-
-  netstat -tulpn | grep :2222
-  ./concourse web \
-    -d \
-    --session-signing-key session_signing_key \
-    --tsa-host-key host_key \
-    --tsa-authorized-keys authorized_worker_keys \
-    --external-url localhost:8080 \
-    --postgres-data-source $postgres_source \
-    --tsa-bind-port=7777
-popd
+./concourse web \
+  -d \
+  --session-signing-key keys/session_signing_key \
+  --tsa-host-key keys/host_key \
+  --tsa-authorized-keys keys/authorized_worker_keys \
+  --external-url localhost:8080 \
+  --postgres-data-source $postgres_source \
+  --tsa-bind-port=7777
